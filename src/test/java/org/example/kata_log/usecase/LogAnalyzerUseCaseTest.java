e726ae1fb22a8e5b0a0d5c28951f2d4594cf3bfb
long.le <long.le@bforbank.com>
2025-05-07 15:39:47 +0200

    shouldReturnLogsByError

--- src/test/java/org/example/kata_log/usecase/LogAnalyzerUseCaseTest.java
+++ src/test/java/org/example/kata_log/usecase/LogAnalyzerUseCaseTest.java
@@ -15,8 +15,20 @@ class LogAnalyzerUseCaseTest {
                 "2025-05-06T10:16:05 ERROR Failed to connect to DB";
 
         useCase = new LogAnalyzer(log);
-        List<LogEntry> warn = useCase.getLogsByLevel(log, "WARN");
+        List<LogEntry> warns = useCase.getLogsByLevel(log, "WARN");
 
-        assertThat(warn).hasSize(1);
+        assertThat(warns).hasSize(1);
+    }
+
+    @Test
+    void shouldReturnLogsByError() {
+        String log = "2025-05-06T10:15:30 WARN High memory usage detected\n" +
+                "2025-05-06T10:16:00 INFO User login successful\n" +
+                "2025-05-06T10:16:05 ERROR Failed to connect to DB";
+
+        useCase = new LogAnalyzer(log);
+        List<LogEntry> errors = useCase.getLogsByLevel(log, "ERROR");
+
+        assertThat(errors).hasSize(1);
     }
 }
